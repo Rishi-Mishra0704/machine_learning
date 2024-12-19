1ff601a7d7f67c704efb4ac2ff5c8d8f13a67efc
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-12-19 22:43:07 +0530

    some change

--- script/loan_data.py
+++ script/loan_data.py
@@ -20,7 +20,6 @@
 #
 # For this project we will be exploring publicly available data from [LendingClub.com](www.lendingclub.com). Lending Club connects people who need money (borrowers) with people who have money (investors). Hopefully, as an investor you would want to invest in people who showed a profile of having a high probability of paying you back. We will try to create a model that will help predict this.
 #
-# Lending club had a [very interesting year in 2016](https://en.wikipedia.org/wiki/Lending_Club#2016), so let's check out some of their data and keep the context in mind. This data is from before they even went public.
 #
 # We will use lending data from 2007-2010 and be trying to classify and predict whether or not the borrower paid back their loan in full. You can download the data from [here](https://www.lendingclub.com/info/download-data.action) or just use the csv already provided. It's recommended you use the csv provided as it has been cleaned of NA values.
 #
@@ -188,6 +187,8 @@ print(classification_report(y_test,rfc_preds))
 print(confusion_matrix(y_test,rfc_preds))
 
 # **What performed better the random forest or the decision tree?**
+
+#
 
 # Random Forest Performed slightly better than Decision Tree in some cases however the decision tree performed better in some cases like recall.Please refer the classification report for more details.
 
